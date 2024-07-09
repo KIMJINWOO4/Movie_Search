404c483a2fc4f22c3a757ec043414ab15d0d65d6
kimjinwoo <rlawlsdn177@naver.com>
2024-07-09 19:03:54 +0900

    style: Loader 스타일 추가

--- src/components/SkeletonLoader.jsx
+++ src/components/SkeletonLoader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SkeletonLoader.css';
 
 const SkeletonLoader = () => {
     return (
         <div className='bg-gray-300 p-4 rounded-md shadow-md animate-pulse'>
             <div className='w-full h-64 bg-gray-400 rounded-md mb-4'></div>
             <div className='h-6 bg-gray-400 rounded-md mb-2'></div>
-            <div className='h-4 bg-gray-400 rounded-md'></div>
+            <div className='h-6 bg-gray-400 rounded-md'></div>
         </div>
     );
 };
